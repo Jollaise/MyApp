0c2bbaef79f0e9ea19ef8eb03783c186c4cd8bd6
Jollaise <julia.reger@me.com>
2018-05-04 13:46:48 +0200

    4.2 added price field, colour field and products layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.where(name: "seed-generated product").destroy_all
+Product.where(name: "Roadster 'Louis'").destroy_all
