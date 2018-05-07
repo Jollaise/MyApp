c9484e1238e8bdddb7ec1a895a14b82622ab6c5b
Jollaise <julia.reger@me.com>
2018-05-08 01:51:51 +0200

    4.6 added order show and index content

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,21 @@
 class OrdersController < ApplicationController
-  
+
   def index
+    @orders = Order.all
   end
 
   def show
-  end
-
-  def new
+    @order = Order.find(params[:id])
   end
 
   def create
   end
 
   def destroy
+  end
+
+  def product_params
+    params.require(:order).permit(:user_id, :product_id, :total)
   end
 
 end
