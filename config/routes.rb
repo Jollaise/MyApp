bd4c7ae8696a5d716474150cce82c9e4d25503e3
Jollaise <julia.reger@me.com>
2018-05-09 18:34:34 +0200

    5.1 changed route_path to about

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
-  root 'simple_pages#simple_pages_about_path'
+  root 'simple_pages#about'
   resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
