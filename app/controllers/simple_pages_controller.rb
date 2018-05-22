7f53829a423b415232da6a892ed09ba12dd70895
Jollaise <julia.reger@me.com>
2018-05-22 15:43:02 +0200

    5.3 added sendgrid for heroku

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -4,4 +4,14 @@ class SimplePagesController < ApplicationController
   def landing_page
     @products = Product.limit(3)
   end
+  def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  @subject = params[:subject]
+  ActionMailer::Base.mail(from: @email,
+      to: 'julia.reger@me.com',
+      subject: @subject,
+      body: @message).deliver_now
+  end
 end
