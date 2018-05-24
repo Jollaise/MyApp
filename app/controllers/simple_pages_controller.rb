f679d0a9476bd90d3b97583576907b44d57667ff
Jollaise <julia.reger@me.com>
2018-05-24 11:48:25 +0200

    5.4 changed link url in application.html

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -11,4 +11,15 @@ class SimplePagesController < ApplicationController
   @subject = params[:subject]
   UserMailer.contact_form(@email, @name, @message).deliver_now
   end
+  def resource_name
+   :user
+ end
+
+ def resource
+   @resource ||= User.new
+ end
+
+ def devise_mapping
+   @devise_mapping ||= Devise.mappings[:user]
+ end
 end
