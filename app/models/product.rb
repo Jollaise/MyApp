3e14db28dd538f0cd37d7d61b96b7d7687ab4a6a
Jollaise <julia.reger@me.com>
2018-05-27 16:17:51 +0200

    5.7 styled show pages and added comment sorting

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,16 @@
 class Product < ApplicationRecord
   has_many :orders
+  has_many :comments
   def self.dev_search(search_term)
     Product.where("name LIKE ?", "%#{search_term}%")
   end
   def self.prod_search(search_term)
     Product.where("name ilike ?", "%#{search_term}%")
+  end
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
+  def lowest_rating_comment
+    comments.rating_asc.first
   end
 end
