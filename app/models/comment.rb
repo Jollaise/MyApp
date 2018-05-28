5c3c3bd9aaa092214e806404b1e9e3fb441c67b8
Jollaise <julia.reger@me.com>
2018-05-28 22:30:00 +0200

    5.9 added will_paginate gem and validations for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,11 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :product
+  validates :body, presence: true
+  validates :user, presence: true
+  validates :product, presence: true
+  validates :rating, numericality: { only_integer: true }, presence: true
   scope :rating_desc, -> { order(rating: :desc) }
   scope :rating_asc, -> { order(rating: :asc) }
+  self.per_page = 2
 end
