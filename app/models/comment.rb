2149d34f5c2ed95a5a29b425942e1f701539536c
Jollaise <julia.reger@me.com>
2018-05-28 22:46:28 +0200

    5.9 deleted self.per_page from model as heroku was showing error

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,5 +7,4 @@ class Comment < ApplicationRecord
   validates :rating, numericality: { only_integer: true }, presence: true
   scope :rating_desc, -> { order(rating: :desc) }
   scope :rating_asc, -> { order(rating: :asc) }
-  self.per_page = 2
 end
