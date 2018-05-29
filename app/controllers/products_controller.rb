fea09c745820514bae4391874731272b86120226
Jollaise <julia.reger@me.com>
2018-05-29 16:54:58 +0200

    changed comment order in show.html

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -21,7 +21,6 @@ class ProductsController < ApplicationController
   # GET /products/1
   # GET /products/1.json
   def show
-
     @comments = @product.comments.paginate(:page => params[:page], :per_page => 2).order('created_at DESC')
   end
 
