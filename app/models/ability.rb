615ac14e35ed4664cc01c39fae4925b9a89172fc
Jollaise <julia.reger@me.com>
2018-05-29 14:46:53 +0200

    5.10 added admin moderation and delete function for comments, restricted acces to admin

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,11 @@ class Ability
   include CanCan::Ability
   def initialize(user)
     user ||= User.new #guest user (not logged in)
-    can :manage, User, id: user.id
+    if user.present?  # additional permissions for logged in users (they can manage their posts)
+      can :manage, User, user_id: user.id 
+      if user.admin?  # additional permissions for administrators
+        can :manage, :all
+      end
+    end
   end
 end
