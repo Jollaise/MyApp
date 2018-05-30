25ed6f583f83e03960ca2a1ae0884b2da75dba9b
Jollaise <julia.reger@me.com>
2018-05-30 16:19:14 +0200

    changed wording for sign up email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,7 +9,7 @@ class UserMailer < ApplicationMailer
   end
 
   def welcome(user)
-  @appname = "Bike Shop"
+  @appname = "Bike Berlin"
   mail(to: user.email,
        subject: "Welcome to #{@appname}!")
   end
