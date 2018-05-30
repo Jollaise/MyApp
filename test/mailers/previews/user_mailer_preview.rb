1d295fbc631809448beb264a409baed5d28b5aaf
Jollaise <julia.reger@me.com>
2018-05-30 16:12:22 +0200

    5.11 added sign up mail and styled sign up mail

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -3,4 +3,7 @@ class UserMailerPreview < ActionMailer::Preview
   def contact_form
     UserMailer.contact_form("john@example.com", "John", "Hello World!")
   end
+  def welcome
+    UserMailer.welcome(User.last)
+  end
 end
