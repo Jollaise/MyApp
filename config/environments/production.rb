8fd6ce9dc48af88eb6114a9ef92d3d0292841781
Jollaise <julia.reger@me.com>
2018-06-20 12:36:58 +0200

    6.13 added configurations for ActionCable to work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,4 +104,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.web_socket_server_url = "wss://shop-bike-berlin.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://shop-bike-berlin.herokuapp.com', 'http://shop-bike-berlin.herokuapp.com']
 end
